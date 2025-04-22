d6b0c1357cdc183e295079eb2f52211fb1cab22d
Jules MICHALAK <jules.michalak@student.junia.com>
2025-04-22 19:52:56 +0200

    require nav et footer pour simplifier chaque page

---
+++ pages/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="footer-content">
+        <p>&copy; 2024 MSD. TOUS DROITS RÉSERVÉS.</p>
+        <div class="socials">
+            <a href="https://soundcloud.com/msdmsd" class="social-icon">SoundCloud</a>
+            <a href="https://www.youtube.com/@Msd-Prime" class="social-icon">YouTube</a>
+            <a href="https://www.instagram.com/msdprod_/?next=%2Fmars.620%2F" class="social-icon">Instagram</a>
+        </div>
+    </div>
+</footer>
--- pages/shop.php
+++ pages/shop.php
@@ -36,69 +36,7 @@ try {
 </head>
 
 <body>
-<nav class="navbar">
-        <div class="utils-co">
-            <a href="connexion.php">
-                <img id="connexion" class="icones" src="../media/icon-account.png" alt="icon-account">
-            </a>
-        </div>
-
-        <div class="logo">
-            <a href="../index.html">
-                <img src="../media/logo_msd.png" alt="Logo">
-            </a>
-        </div>
-
-        <div class="utils-ca">
-            <button popovertarget="cart" popovertargetaction="show" class="button">
-                <img id="panier" class="icones" src="../media/icon-cart.png" alt="Panier">
-            </button>
-        </div>
-
-        <nav popover id="cart">
-            <button popovertarget="cart" popovertargetaction="hide" class="button close-button">×</button>
-            <div class="cart-item">
-                <img src="../media/merch/vinyl1.png" alt="Vinyl 2">
-                <div class="item-details">
-                    <p>ROCKSTAR Vinyle EDITION DELUXE</p>
-                    <p>Prix: 39,99 €</p>
-                </div>
-            </div>
-            <div class="cart-item">
-                <img src="../media/merch/Pull_noir_devant.png" alt="Pull 1">
-                <div class="item-details">
-                    <p>Pull Noir Msd</p>
-                    <p>Prix: 74,99 €</p>
-                    <p>Taille: L</p>
-                </div>
-            </div>
-            <div class="cart-item">
-                <img src="../media/merch/affiche-normall.png" alt="Poster 2">
-                <div class="item-details">
-                    <p>Pack de poster Msd x3</p>
-                    <p>Prix: 19,99 €</p>
-                </div>
-            </div>
-            <div class="cart-item">
-                <img src="../media/merch/Teeshirt_b_devant.png" alt="T-shirt 2">
-                <div class="item-details">
-                    <p>T-shirt blanc Msd</p>
-                    <p>Prix: 24,99 €</p>
-                    <p>Taille: XL</p>
-                </div>
-            </div>
-            <div class="checkout">
-                <p>Total: 159,96 €</p>
-                <button>Procéder au paiement</button>
-            </div>
-        </nav>
-
-        <div class="navbar-menu">
-            <a href="accueil.php">ACCUEIL</a>
-            <a href="shop.php">BOUTIQUE</a>
-            <a href="contact.html">CONTACT</a>
-        </div>
-    </nav>
+<?php require_once 'navbar.php'; ?>
 
     <div class="conteneur-shop">
         <div class="shop-grid">
@@ -152,15 +90,7 @@ try {
         </div>
     <?php endforeach; ?>
 
-    <footer class="footer">
-        <div class="footer-content">
-            <p>&copy; 2024 MSD. TOUS DROITS RÉSERVÉS.</p>
-            <div class="socials">
-                <a href="https://soundcloud.com/msdmsd" class="social-icon">SoundCloud</a>
-                <a href="https://www.youtube.com/@Msd-Prime" class="social-icon">YouTube</a>
-                <a href="https://www.instagram.com/msdprod_/?next=%2Fmars.620%2F" class="social-icon">Instagram</a>
-            </div>
-        </div>
-    </footer>
+    <?php require_once 'footer.php'; ?>
+
 </body>
 </html>
