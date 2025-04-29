5d4513813c4014ce0eaffd076984b50f32c5b4fb
Alex <144245061+alex-paolo-CIR@users.noreply.github.com>
2025-04-29 12:03:09 +0200

    cc

--- traitements/logout.php
+++ traitements/logout.php
@@ -1,9 +1,16 @@
 <?php
 session_start();
+require("db.php");
 
-session_unset();
+if (isset($_SESSION['id_user'])) {
+    $stmt = $conn->prepare("UPDATE utilisateurs SET remember_token = NULL WHERE id = :id");
+    $stmt->execute([':id' => $_SESSION['id_user']]);
+}
+
 session_destroy();
 
-header("Location: ../pages/accueil.php");
+setcookie('remember_token', '', time() - 3600, '/');
+
+header("Location: ../pages/connexion.php");
 exit();
 ?>
